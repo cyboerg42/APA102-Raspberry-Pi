c46096cbc7afa5e3ddaa77678bdbcc2948790a57
XDjackieXD <jakob.riepler@gmail.com>
2015-05-04 23:13:46 +0200

    Program is working! :D (except for the pong generator...)

--- frame_bin.c
+++ frame_bin.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
 	}
 
 	int a, frames, del, leds, count;
-	uint8_t frame[30000], buffer[4];
+	uint8_t frame[3000], buffer[4], tmp[1];
 	
 	FILE * fp;
 	fp = fopen (argv[1], "r");
@@ -25,26 +25,35 @@ int main(int argc, char *argv[]){
 	fread(&del, 4, 1, fp);
 	fread(&leds, 4, 1, fp);
 
+	printf("Frames: %d\nDelay: %d\nLEDs: %d", frames, del, leds);
+
 	for(count = 0; count < frames; count++){
 		fread(frame, 1, 3*leds, fp);
-
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
+		tmp[0]=0;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=0;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=0;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=0;		
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
 	    
 		for(a=0; a < (leds*3); a=a+3){
 			buffer[0]=0b11111111;
-			buffer[1]=frame[a];
+			buffer[1]=frame[a+2];
 			buffer[2]=frame[a+1];
-			buffer[3]=frame[a+2];
+			buffer[3]=frame[a];
 			wiringPiSPIDataRW(0, (unsigned char*)buffer, 4);
 		}
 	
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
-		wiringPiSPIDataRW(0, (unsigned char*)0b11111111, 1);
+		tmp[0]=255;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=255;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=255;
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
+		tmp[0]=255;		
+		wiringPiSPIDataRW(0, (unsigned char*)tmp, 1);
 		delay(del);
 	}
 
---
+++ whitecreator.c
@@ -0,0 +1,37 @@
+//Program to create a simple pong file with the new file format
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(int argc, char *argv[]){
+
+	if(argc < 2 || argc > 2){
+		printf("usage: %s file\n", argv[0]);
+		return -1;
+	}
+
+	FILE *fp = fopen(argv[1], "w");
+
+	if(fp == NULL){
+		printf("Error opening file \"%s\"!\n", argv[1]);
+		return -1;
+	}
+
+	int delay=200, frames=1, leds=300, count;
+
+	fwrite(&frames, 4, 1, fp);
+	fwrite(&delay, 4, 1, fp);
+	fwrite(&leds, 4, 1, fp);
+
+	for(count = 0; count < leds; count++){
+		fputc(255, fp);
+		fputc(255, fp);
+		fputc(255, fp);
+	}
+
+	fclose(fp);
+
+	return 0;
+
+}
